7925ebb5af20b4d88ac006b9afaf8c3daf135f7f
AnDreV133 <dmitrijkubik@gmail.com>
2023-12-16 15:59:04 +0300

    writing UndoRedo class

--- src/main/java/src/UndoRedo.java
+++ src/main/java/src/UndoRedo.java
@@ -4,36 +4,51 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class UndoRedo {
+    private static final int CAPACITY = 30;
     private ArrayList<ArrayList<Float>> lastBackground;
     private ArrayList<ArrayList<ShapeId>> lastForeground;
-    private LinkedList<PairGrounds> sequenceStates;
+    private LinkedList<PairGrounds> stackDo;
+    private LinkedList<PairGrounds> subStackDo;
+
+    public UndoRedo(ArrayList<ArrayList<Float>> lastBackground, ArrayList<ArrayList<ShapeId>> lastForeground) {
+        this.lastBackground = lastBackground;
+        this.lastForeground = lastForeground;
+    }
 
     private void add(PairGrounds pairGrounds) {
-        sequenceStates.add(pairGrounds);
+        if (stackDo.size() == CAPACITY) {
+            var temp = stackDo.getFirst();
+            if (temp.background() != null) lastBackground = temp.background();
+            else if (temp.foreground() != null) lastForeground = temp.foreground();
+
+            stackDo.removeFirst();
+        }
+
+        stackDo.add(pairGrounds);
     }
 
     public void addBackground(ArrayList<ArrayList<Float>> background) {
-        sequenceStates.add(new PairGrounds(new ArrayList<>(background), null));
+        stackDo.add(new PairGrounds(new ArrayList<>(background), null));
     }
 
     public void addForeground(ArrayList<ArrayList<ShapeId>> foreground) {
-        sequenceStates.add(new PairGrounds(null, new ArrayList<>(foreground)));
+        stackDo.add(new PairGrounds(null, new ArrayList<>(foreground)));
     }
 
     public PairGrounds getPairGrounds() {
-        var res = sequenceStates.getLast();
+        var res = stackDo.getLast();
         var tempForeground = res.foreground();
         var tempBackground = res.background();
 
-        int i = sequenceStates.size() - 2;
+        int i = stackDo.size() - 2;
         while (tempBackground == null || tempForeground == null) {
             if (i < 0) {
                 if (tempBackground == null) tempBackground = lastBackground;
-                else if (tempForeground == null) tempForeground = lastForeground;
+                else tempForeground = lastForeground;
                 break;
             }
 
-            var iterPairGrounds = sequenceStates.get(i);
+            var iterPairGrounds = stackDo.get(i);
             if (res.background() == null && tempBackground != null)
                 tempBackground = iterPairGrounds.background();
             else if (res.foreground() == null && tempForeground != null)
@@ -45,5 +60,11 @@ public class UndoRedo {
         return new PairGrounds(tempBackground, tempForeground);
     }
 
+    public PairGrounds redo() {
 
+    } // ctrl + z
+
+    public PairGrounds undo() {
+
+    }  // ctrl + shift + z
 }
